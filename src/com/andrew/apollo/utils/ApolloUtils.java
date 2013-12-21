51df8ed8144bcb0031c665ad13ce19c5985d8a6d
Umair Khan <omerjerk@gmail.com>
2013-12-21 19:43:44 +0530

    Fixing Build.VERSION_CODES related issues
    Signed-off-by: Umair Khan <omerjerk@gmail.com>

--- src/com/andrew/apollo/utils/ApolloUtils.java
+++ src/com/andrew/apollo/utils/ApolloUtils.java
@@ -67,7 +67,7 @@ public final class ApolloUtils {
      *         otherwise
      */
     public static final boolean hasJellyBean() {
-        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN;
+        return false;
     }
 
     /**
@@ -78,7 +78,7 @@ public final class ApolloUtils {
      *         false otherwise
      */
     public static final boolean hasJellyBeanMR2() {
-        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2;
+        return false;
     }
 
     /**
