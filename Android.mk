a972442cbfad108773b90141b2ab0cce117fbb50
Umair Khan <omerjerk@gmail.com>
2013-12-21 19:37:02 +0530

    Fixing annotations errors
    Signed-off-by: Umair Khan <omerjerk@gmail.com>

--- Android.mk
+++ Android.mk
@@ -6,8 +6,11 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES := src/com/andrew/apollo/IApolloService.aidl
 LOCAL_SRC_FILES += $(call all-java-files-under, src)
 
+
+
 LOCAL_STATIC_JAVA_LIBRARIES := \
-    android-support-v4
+    android-support-v4 \
+    annotations-support
 
 LOCAL_PACKAGE_NAME := Apollo
 LOCAL_OVERRIDES_PACKAGES := Music
@@ -16,4 +19,11 @@ LOCAL_SDK_VERSION := current
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
 include $(BUILD_PACKAGE)
+
+##################################################
+include $(CLEAR_VARS)
+
+LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := annotations-support:annotations.jar
+
+include $(BUILD_MULTI_PREBUILT)
 
